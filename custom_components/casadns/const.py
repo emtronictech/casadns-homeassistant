0847bb94e46b3651c62700e2d5b062c95f9ef935
EMTRONIC <erik@emtronic.tech>
2025-11-23 09:39:10 +0100

    Support for IPv4 and IPv6

--- custom_components/casadns/const.py
+++ custom_components/casadns/const.py
@@ -1,12 +1,15 @@
+from __future__ import annotations
+
 DOMAIN = "casadns"
 
 CONF_DOMAINS = "domains"
 CONF_TOKEN = "token"
-CONF_INTERVAL = "interval"  # minutes
+CONF_INTERVAL = "interval"
 
-DEFAULT_INTERVAL = 15  # minutes
+DEFAULT_INTERVAL = 15
 
-ATTR_PUBLIC_IP = "public_ip"
+ATTR_PUBLIC_IPV4 = "public_ipv4"
+ATTR_PUBLIC_IPV6 = "public_ipv6"
 ATTR_LAST_STATUS = "last_status"
 ATTR_LAST_ERROR = "last_error"
 ATTR_LAST_UPDATED = "last_updated"
