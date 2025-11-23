46297eb1cef1a46ebf5c96e6fcd5e54bc9869154
EMTRONIC <erik@emtronic.tech>
2025-11-23 11:06:31 +0100

    Fix IPv6

--- custom_components/casadns/const.py
+++ custom_components/casadns/const.py
@@ -8,8 +8,7 @@ CONF_INTERVAL = "interval"
 
 DEFAULT_INTERVAL = 15
 
-ATTR_PUBLIC_IPV4 = "public_ipv4"
-ATTR_PUBLIC_IPV6 = "public_ipv6"
+ATTR_PUBLIC_IP = "public_ip"
 ATTR_LAST_STATUS = "last_status"
 ATTR_LAST_ERROR = "last_error"
 ATTR_LAST_UPDATED = "last_updated"
